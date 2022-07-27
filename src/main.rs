c9f6fe3e2ac2e999fee222867dd104afdb75187d
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2022-07-26 22:34:57 -0300

    Add initial main implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use dotenv;
+use std::env;
+use yaid::Bot;
+
 fn main() {
-    println!("Hello, world!");
+    dotenv::dotenv().ok();
+
+    let token = env::var("TOKEN").expect("TOKEN must be set!");
+
+    let bot: &'static Bot = Bot::new(&token).expect("Invalid token!");
+
+    bot.get_updates();
 }
