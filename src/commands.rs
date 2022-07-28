7529b322d7faada1553d09f0ef657f6782891345
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2022-07-28 01:20:04 -0300

    Update how the execution of commands are handled in commands

--- src/commands.rs
+++ src/commands.rs
@@ -1,47 +1,31 @@
-use frankenstein::{Api, Message, TelegramApi};
-use frankenstein::SendMessageParams;
+mod download;
+mod help;
+mod start;
 
-fn send_message(api: &Api, message: &Message, text: &str) {
-    let send_message_params = SendMessageParams::builder()
-        .chat_id(message.chat.id)
-        .text(text)
-        .build();
+use frankenstein::{Api, Message};
+use reqwest::Client;
 
-    if let Err(error) = api.send_message(&send_message_params) {
-        panic!("Failed to send message: {:?}", error);
+pub fn has_valid_command<'a, 'b>(message: &'a Message) -> Result<&'b str, ()> {
+    let text = match message.text.as_ref() {
+        Some(text) => text,
+        None => return Err(()),
+    };
+
+    let command = text.split(" ").collect::<Vec<&str>>()[0];
+
+    match command {
+        "/start" => Ok("start"),
+        "/download" => Ok("download"),
+        "/help" => Ok("help"),
+        _ => Err(()),
     }
 }
 
-fn start(api: &Api, message: Message) {
-    // Todo start command for /start
-
-    send_message(api, &message, "Start message!");
-}
-
-fn download(api: &Api, message: Message) {
-    // Todo download command for /download url
-
-    send_message(api, &message, "Download message!");
-}
-
-fn help(api: &Api, message: Message) {
-    // Todo help command for /help
-
-    send_message(api, &message, "Help message!");
-}
-
-pub fn has_valid_command(message: &Message) -> Result<Box<dyn FnOnce(&Api, Message) + Send>, ()> {
-    let text = match message.text.as_ref() {
-        Some(text) => text,
-        None => return Err(())
-    };
-    
-    let message_content = text.split(" ").collect::<Vec<&str>>()[0];
-
-    match message_content {
-        "/start" => Ok(Box::new(start)),
-        "/download" => Ok(Box::new(download)),
-        "/help" => Ok(Box::new(help)),
-        _ => Err(())
+pub async fn execute_command(api: &Api, client: &Client, command: &str, message: Message) {
+    match command {
+        "start" => start::execute(api, message),
+        "download" => download::execute(api, client, message).await,
+        "help" => help::execute(api, message),
+        _ => unreachable!(),
     }
 }
