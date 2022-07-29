29dc6895b1c95f9a1f94af0476e81c9d0f5580a5
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2022-07-29 12:33:07 -0300

    Finish start command

--- src/commands/start.rs
+++ src/commands/start.rs
@@ -1,5 +1,10 @@
 use frankenstein::Message;
 
 pub fn execute(bot: &crate::Bot, message: Message) {
-    bot.send_message(message.chat.id, "Start message!");
+    bot.send_message(
+        message.chat.id,
+        "Hi, my name is YAID and I can download Instagram posts! \
+        It's simple, you just have to type /download <post_url> and the rest is my responsability, no need to worry! \
+        But remember: I can't send to you posts from a private profile.",
+    );
 }
