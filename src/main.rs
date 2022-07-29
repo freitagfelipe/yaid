1ec455c72cb3311f2c2c5fbcfefd8b8ab8fecffc
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2022-07-29 15:02:31 -0300

    Add a setup function to check environment variables in main

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,19 @@ use yaid::Bot;
 
 #[tokio::main]
 async fn main() {
-    dotenv::dotenv().ok();
+    setup();
 
-    let token = env::var("TOKEN").expect("TOKEN must be set!");
+    let token = env::var("TOKEN").unwrap();
 
     let bot: &'static Bot = Bot::new(&token).expect("Invalid token or no internet connection!");
 
     bot.get_updates();
+}
+
+fn setup() {
+    dotenv::dotenv().ok();
+
+    env::var("TOKEN").expect("TOKEN must be set!");
+    env::var("URL").expect("URL must be set!");
+    env::var("AUTH_TOKEN").expect("AUTH_TOKEN must be set!");
 }
