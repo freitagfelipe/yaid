71950415b16162cb728a4eda5d49f22f7e11c24d
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2022-07-29 12:33:48 -0300

    Finish help command

--- src/commands/help.rs
+++ src/commands/help.rs
@@ -1,5 +1,10 @@
 use frankenstein::Message;
 
 pub fn execute(bot: &crate::Bot, message: Message) {
-    bot.send_message(message.chat.id, "Help message!");
+    bot.send_message(
+        message.chat.id,
+        "I have just one command, /download! \
+        And it's really simple to use: type /download <post_url>, \
+        and remember that I can't send to you posts from private profiles!",
+    );
 }
