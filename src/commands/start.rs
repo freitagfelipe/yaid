361daee29c8bea3d586d3dd7bfb465d426ad53dd
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2022-08-24 00:42:21 -0300

    Update the message from the help and start commands

--- src/commands/start.rs
+++ src/commands/start.rs
@@ -5,8 +5,8 @@ pub fn execute(bot: &crate::Bot, message: Message) {
         message.chat.id,
         "Hi, my name is YAID and I can download Instagram posts and stories! \
         It's simple, I only have three easy-to-use commands:
-        ∙ /download_post <post_link>
-        ∙ /download_stories <user>
+        ∙ /download_post <post-link>
+        ∙ /download_stories <user-name>
         ∙ /help\n\
         You just have to type the command followed by the requested parameter when needed and the rest is my responsability, no need to worry! \
         But remember: I can't send to you posts and stories from private profiles.",
